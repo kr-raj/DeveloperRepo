cb82b35be8aa10fa31da52d63be159ea691b7667
kr-raj <rajkumar@3clogic.com>
2019-09-18 13:21:13 +0530

    sync

--- lambda3/index.js
+++ lambda3/index.js
@@ -1,9 +1,51 @@
-var time = require('time');
-exports.handler = (event, context, callback) => {
-    var currentTime = new time.Date(); 
-    currentTime.setTimezone("America/Los_Angeles");
-    callback(null, {
-        statusCode: '200',
-        body: 'The time in Los Angeles is: ' + currentTime.toString(),
-    });
+'use strict';
+console.log('Loading hello world function');
+ 
+exports.handler = async (event) => {
+    let name = "Jhon";
+    let city = 'World';
+    let time = 'day';
+    let day = '';
+    let responseCode = 200;
+    console.log("request: " + JSON.stringify(event));
+    
+    if (event.queryStringParameters && event.queryStringParameters.name) {
+        console.log("Received name: " + event.queryStringParameters.name);
+        name = event.queryStringParameters.name;
+    }
+    
+    if (event.queryStringParameters && event.queryStringParameters.city) {
+        console.log("Received city: " + event.queryStringParameters.city);
+        city = event.queryStringParameters.city;
+    }
+    
+    if (event.headers && event.headers['day']) {
+        console.log("Received day: " + event.headers.day);
+        day = event.headers.day;
+    }
+    
+    if (event.body) {
+        let body = JSON.parse(event.body)
+        if (body.time) 
+            time = body.time;
+    }
+ 
+    let greeting = `Good ${time}, ${name} of ${city}.`;
+    if (day) greeting += ` Happy ${day}!`;
+
+    let responseBody = {
+        message: greeting,
+        input: event
+    };
+    
+
+    let response = {
+        statusCode: responseCode,
+        headers: {
+            "x-custom-header" : "my custom header value"
+        },
+        body: JSON.stringify(responseBody)
+    };
+    console.log("response: " + JSON.stringify(response))
+    return response;
 };
